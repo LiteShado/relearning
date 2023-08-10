b0888b81caa4dca779dd10753a0b96fbcb853c47
Natalie <theliteshado@icloud.com>
2023-08-10 04:06:26 -0400

    exercises

--- relearning.js
+++ relearning.js
@@ -157,3 +157,80 @@ function caseInSwitch(val) {
 }
 
 caseInSwitch(1);
+
+//default statement
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+      return "apple";
+      break;
+    case "b":
+      return "bird";
+      break;
+    case "c":
+      return "cat";
+      break;
+    default:
+      return "stuff";
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+//blackjack code with conditionals
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  console.log(count);
+
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+  // Only change code above this line
+}
+
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("A");
+
+//introduction to objects and properties
+
+// Setup
+const testObj = {
+  hat: "ballcap",
+  shirt: "jersey",
+  shoes: "cleats",
+};
+
+// Only change code below this line
+const hatValue = testObj.hat; // Change this line
+const shirtValue = testObj.shirt; // Change this line
