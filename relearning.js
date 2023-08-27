44933eb382902f7716cb075d75c90911b1eb4f5f
Natalie <theliteshado@icloud.com>
2023-08-27 13:57:39 -0400

    for loops

--- relearning.js
+++ relearning.js
@@ -407,3 +407,13 @@ while (i >= 0) {
   myArray.push(i);
   i--;
 }
+
+// For loops
+// Setup
+const myArray = [];
+
+// Only change code below this line
+
+for (let i = 1; i < 5; i++) {
+  myArray.push(i);
+}
