9acce47aeccbe8f7a84f6297d725c950498f0a72
Natalie <theliteshado@icloud.com>
2023-08-27 13:59:31 -0400

    odd numbers for loop

--- relearning.js
+++ relearning.js
@@ -417,3 +417,12 @@ const myArray = [];
 for (let i = 1; i < 5; i++) {
   myArray.push(i);
 }
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+
+for (let i = 1; i <= 9; i += 2) {
+  myArray.push(i);
+}
