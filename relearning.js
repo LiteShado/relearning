6a6c386793a312948b23757d5c36f342120acc75
Natalie <theliteshado@icloud.com>
2023-10-06 23:42:22 -0400

    getters and setters

--- relearning.js
+++ relearning.js
@@ -821,24 +821,45 @@ const createPerson = (name, age, gender) =>
   // Only change code below this line
   ({ name, age, gender }); //can change variable name to whatever necessary
 
+//implicit and explicit classes and constructors
 
-  //implicit and explicit classes and constructors
-
-  // Only change code below this line
+// Only change code below this line
 class Vegetable {
-  constructor(name){
+  constructor(name) {
     this.name = name;
   }
 }
 // Only change code above this line
 
-
 class Fruit {
-  something(){
+  something() {
     console.log("you got it!");
   }
 }
-const carrot = new Vegetable('carrot');
+const carrot = new Vegetable("carrot");
 const fruity = new Fruit();
 console.log(carrot.name); // Should display 'carrot'
 console.log(fruity.something); //should display "you got it!"
+
+//getters and setters
+
+// Only change code below this line
+class Thermostat {
+  constructor(fahrenheit) {
+    this._fahrenheit = fahrenheit;
+  }
+
+  get temperature() {
+    return (5 / 9) * (this._fahrenheit - 32);
+  }
+
+  set temperature(celsius) {
+    return (this._fahrenheit = (celsius * 9.0) / 5 + 32);
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
