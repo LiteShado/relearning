6a09abacec4f6229a0cb5f0949f9f74e8b55c7fd
Natalie <theliteshado@icloud.com>
2023-10-09 21:15:15 -0400

    check for username parameters

--- relearning.js
+++ relearning.js
@@ -1039,3 +1039,16 @@ let noNumRegex = /\D/g; // looks for all non-numerical characters, which is the 
 let nonnumbers = movie.match(noNumRegex).length;
 
 console.log(nonnumbers);
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i; // Usernames can only use alpha-numeric characters.
+
+//The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+
+//Username letters can be lowercase and uppercase.
+
+//Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters
+
+let checkUser = userCheck.test(username);
+
+console.log(checkUser);
